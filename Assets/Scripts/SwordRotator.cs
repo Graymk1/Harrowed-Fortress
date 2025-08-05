055393e9261bfdfac62ec10b0410b010f8b8e3a1
Graymk1 <gmk8438@nyu.edu>
2025-08-05 13:04:08 -0400

    a

--- Assets/Scripts/SwordRotator.cs
+++ Assets/Scripts/SwordRotator.cs
@@ -6,9 +6,10 @@ public class SwordRotator : MonoBehaviour
 {
     
 
-    public float hitTime = 2f;
-    public float maxHittime = 2f;
     public GameObject sword;
+    public GameObject visualSword;
+    private bool debounce = true;
+    
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -27,8 +28,10 @@ public class SwordRotator : MonoBehaviour
 
         float angle = Mathf.Atan2(mouseDir.y, mouseDir.x) * Mathf.Rad2Deg;
 
-        if (Input.GetKeyDown(KeyCode.Mouse0))
+        if (Input.GetKeyDown(KeyCode.Mouse0) && debounce)
         {
+            debounce = false;
+            print(mouseDir);
             StartCoroutine(UseSword());
         }
 
@@ -36,15 +39,26 @@ public class SwordRotator : MonoBehaviour
         {
             if (PlayerMovement.instance.transform.localScale.x == 1)
             {
+                
+                sword.SetActive(true);
+                visualSword.SetActive(false);
                 transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, angle));
-                yield return new WaitForSeconds(1f);
+                yield return new WaitForSeconds(0.5f);
                 transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, 0));
+                sword.SetActive(false);
+                visualSword.SetActive(true);
+                debounce = true;
             }
             else
             {
+                sword.SetActive(true);
+                visualSword.SetActive(false);
                 transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, angle + 180f));
-                yield return new WaitForSeconds(1f);
+                yield return new WaitForSeconds(0.5f);
                 transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, 0));
+                sword.SetActive(false);
+                visualSword.SetActive(true);
+                debounce = true;
             }
 
 
--- Assets/Scripts/SwordScript.cs
+++ Assets/Scripts/SwordScript.cs
@@ -4,12 +4,28 @@ using UnityEngine;
 
 public class SwordScript : MonoBehaviour
 {
-    
+    public static SwordScript instance;
+
+
+    public float damage = 1f;
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        instance = this;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Enemy"))
+        {
+            collision.GetComponent<Health>().CurHealth -= damage;
+            if (collision.GetComponent<Health>().CurHealth <= 0)
+            {
+                Destroy(collision.gameObject);
+            }
+        }
     }
 
     // Update is called once per frame
