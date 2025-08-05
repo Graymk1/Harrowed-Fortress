6205e678185807fd16301e03304a17129c792531
AllenC90 <s108059_chouyulun@btsflip.com>
2025-08-05 16:03:27 -0400

    changed player movement alot

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -10,40 +10,48 @@ public class PlayerMovement : MonoBehaviour
 
     public static PlayerMovement instance;
     public Transform groundCheckPoint;
-
     public LayerMask groundLayer;
+
     private float groundCheckRadius = 0.2f;
+
     [Header("Jump Setting")]
     public float jumpHeight = 16f;
     public int maxJumps = 1;
-    public int maxJump = 1;
-    public int numJumps = 0;
+    private int numJumps;
+
     [Header("Dash Setting")]
     public int maxDashes = 1;
-    public int numDashes = 1;
-
+    private int numDashes;
     public float dashStrength = 10f;
     public float dashCooldown = 1f;
     private bool isDashing = false;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private float defaultGravityScale;
+
     void Start()
     {
         instance = this;
+        defaultGravityScale = rb.gravityScale;
     }
 
-
-    bool GroundCheck()
-    {
-        return Physics2D.OverlapCircle(groundCheckPoint.position, groundCheckRadius, groundLayer);
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        float horizontalInput = Input.GetAxisRaw("Horizontal") * Time.deltaTime * walkSpeed;
-        transform.Translate(horizontalInput, 0, 0);
+        HandleMovement();
+        HandleJump();
+        HandleDash();
+    }
 
+    void HandleMovement()
+    {
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        Vector2 velocity = rb.linearVelocity;
+
+        if (!isDashing)
+            velocity.x = horizontalInput * walkSpeed;
+
+        rb.linearVelocity = velocity;
+
+        // Flip player sprite
         if (horizontalInput > 0 && swordRotator.transform.rotation.z == 0)
         {
             transform.localScale = new Vector3(1, 1, 1);
@@ -52,46 +60,46 @@ public class PlayerMovement : MonoBehaviour
         {
             transform.localScale = new Vector3(-1, 1, 1);
         }
+    }
 
-        float nextVelocityX = horizontalInput * walkSpeed;
-        float nextVelocityY = rb.linearVelocityY;
-
-        if (GroundCheck() && nextVelocityY <= 0)
+    void HandleJump()
+    {
+        if (GroundCheck())
         {
             numJumps = maxJumps;
         }
 
-        if (Input.GetKeyDown(KeyCode.Space) && numJumps > 0 && nextVelocityY <= 0)
+        if (Input.GetKeyDown(KeyCode.Space) && numJumps > 0)
         {
-            nextVelocityY = jumpHeight;
-            numJumps -= 1;
-        }
-        if (isDashing == false)
-        {
-            rb.linearVelocity = new Vector2(nextVelocityX, nextVelocityY);
-        }
-
-        if (Input.GetKeyDown(KeyCode.LeftShift))
-        {
-            isDashing = true;
-            StartCoroutine(Dash());
-        }
-
-        IEnumerator Dash()
-        {
-            Debug.Log("Before: " + rb.linearVelocity);
-
-            // Forcefully override x velocity
-            rb.gravityScale = 0;
-            rb.AddForce(new(transform.localScale.x * dashStrength, 0), ForceMode2D.Impulse);
-
-            rb.linearVelocityY = 0f;
-            yield return new WaitForSeconds(.15f);
-            rb.gravityScale = 3;
-            Debug.Log("After: " + rb.linearVelocity);
-            isDashing = false;
-
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpHeight);
+            numJumps--;
         }
     }
 
+    void HandleDash()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftShift) && !isDashing)
+        {
+            StartCoroutine(Dash());
+        }
+    }
+
+    IEnumerator Dash()
+    {
+        isDashing = true;
+        float originalGravity = rb.gravityScale;
+        rb.gravityScale = 0f;
+
+        rb.linearVelocity = new Vector2(transform.localScale.x * dashStrength, 0f);
+
+        yield return new WaitForSeconds(0.15f);
+
+        rb.gravityScale = defaultGravityScale;
+        isDashing = false;
+    }
+
+    bool GroundCheck()
+    {
+        return Physics2D.OverlapCircle(groundCheckPoint.position, groundCheckRadius, groundLayer);
+    }
 }
