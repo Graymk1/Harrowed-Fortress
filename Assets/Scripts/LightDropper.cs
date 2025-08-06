4a1c07d098ec100c80d2a52b67b3362c942615e6
Graymk1 <gmk8438@nyu.edu>
2025-08-06 13:38:35 -0400

    a

---
+++ Assets/Scripts/LightDropper.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class LightDropper : MonoBehaviour
+{
+    public static LightDropper instance;
+
+    public float lightSpeed = 10f;
+    public Rigidbody2D rb;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        instance = this;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 playerDir = PlayerMovement.instance.transform.position - transform.position;
+        rb.linearVelocity = playerDir.normalized * lightSpeed;
+
+    }
+}
