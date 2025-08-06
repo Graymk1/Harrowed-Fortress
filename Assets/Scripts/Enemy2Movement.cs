836c5cdfcb772a6f411f0c77f7d8252ac06036a4
Graymk1 <gmk8438@nyu.edu>
2025-08-06 15:54:31 -0400

    a

--- Assets/Scripts/Enemy2Movement.cs
+++ Assets/Scripts/Enemy2Movement.cs
@@ -2,7 +2,8 @@ using UnityEngine;
 
 public class Enemy2Movement : MonoBehaviour
 {
-    public float fallSpeed = 5f;
+    public Rigidbody2D rb;
+    public float flySpeed = 5f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -13,6 +14,7 @@ public class Enemy2Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position += new Vector3(0,fallSpeed * Time.deltaTime * -1,0);
+        Vector2 moveDir = PlayerMovement.instance.transform.position - transform.position;
+        rb.linearVelocity = moveDir.normalized * flySpeed;
     }
 }
