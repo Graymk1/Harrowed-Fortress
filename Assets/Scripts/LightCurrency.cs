ea8a67987edeb72859ed088eb5ba2357507a151e
AllenC90 <s108059_chouyulun@btsflip.com>
2025-08-06 15:37:23 -0400

    light timer

--- Assets/Scripts/LightCurrency.cs
+++ Assets/Scripts/LightCurrency.cs
@@ -4,10 +4,21 @@ public class LightCurrency : MonoBehaviour
 {
     public static LightCurrency Instance;
     public int LightCurrencyCount;
+    public float lightTimer;
     void Start()
     {
         Instance = this;
     }
+    void Update()
+    {
+        lightTimer += Time.deltaTime;
+        if (lightTimer >= 2)
+        {
+            ChangeLightCurrency(-1);
+            lightTimer = 0;
+        }
+    }
+
     public void ChangeLightCurrency(int amount)
     {
         LightCurrencyCount += amount;
