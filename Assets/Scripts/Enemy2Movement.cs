8a89e592a10a50bf080acb1ad56570500a18c2cd
AllenC90 <s108059_chouyulun@btsflip.com>
2025-08-07 10:15:59 -0400

    flying enemy move

--- Assets/Scripts/Enemy2Movement.cs
+++ Assets/Scripts/Enemy2Movement.cs
@@ -3,18 +3,26 @@ using UnityEngine;
 public class Enemy2Movement : MonoBehaviour
 {
     public float Speed = 5f;
+    public float ChaseDis;
     public Rigidbody2D rb;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
         Vector2 playerDir = PlayerMovement.instance.transform.position - transform.position;
-        rb.linearVelocity = playerDir.normalized * Speed;
+        if (playerDir.magnitude < ChaseDis)
+        {
+            rb.linearVelocity = playerDir.normalized * Speed;
+        }
+        else
+        {
+            rb.linearVelocity = Vector2.zero;
+        }
     }
 }
