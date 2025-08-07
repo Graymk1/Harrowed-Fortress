0d17fbfffbe50a62be2aac0cfdbd71b99f4dde72
AllenC90 <s108059_chouyulun@btsflip.com>
2025-08-07 10:38:32 -0400

    flying enemy

--- Assets/Scripts/Enemy2Movement.cs
+++ Assets/Scripts/Enemy2Movement.cs
@@ -6,10 +6,14 @@ public class Enemy2Movement : MonoBehaviour
     public float ChaseDis;
     public Rigidbody2D rb;
 
+    public int damage = 1;
+    public float DamageCd = 0.2f;
+    public float DamageTimer;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -23,6 +27,23 @@ public class Enemy2Movement : MonoBehaviour
         else
         {
             rb.linearVelocity = Vector2.zero;
+        }
+    }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        DamageTimer = 0;
+    }
+    void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            DamageTimer -= Time.deltaTime;
+            if (DamageTimer < 0)
+            {
+                collision.gameObject.GetComponent<LightCurrency>().ChangeLightCurrency(-damage);
+                collision.gameObject.GetComponent<Animator>().SetTrigger("Hurt");
+                DamageTimer = DamageCd;
+            }
         }
     }
 }
