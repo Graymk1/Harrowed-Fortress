4dbdedfd05388cec20f56b396baf519c8f35dfa6
AllenC90 <s108059_chouyulun@btsflip.com>
2025-08-08 11:18:38 -0400

    main menu

---
+++ Assets/UIMan.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEditor;
+
+public class UIMan : MonoBehaviour
+{
+    public Button PlayButton, QuitButton;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        PlayButton.onClick.AddListener(Play);
+        QuitButton.onClick.AddListener(Quit);
+    }
+
+    public void Play()
+    {
+        SceneManager.LoadScene("Tutorial");
+    }
+    public void Quit()
+    {
+        Application.Quit();
+        EditorApplication.isPlaying = false;
+    }
+}
